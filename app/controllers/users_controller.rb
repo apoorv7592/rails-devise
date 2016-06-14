565b529ce93445bb77d80a8f831136765223f0e5
ag7592 <apoorv.g@bewakoof.com>
2016-06-14 21:40:08 +0530

    login removed on user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@
 #
 
 class UsersController < ApplicationController 
-  before_filter :authenticate_user!, only: [:show, :edit, :update, :destroy]  
+  #before_filter :authenticate_user!, only: [:show, :edit, :update, :destroy]  
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
