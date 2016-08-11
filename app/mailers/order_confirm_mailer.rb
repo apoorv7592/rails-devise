e028816c71cadbd31d46d935fff42fff4876cc4b
ag7592 <apoorv.g@bewakoof.com>
2016-08-11 12:32:51 +0530

    mailer changes

--- app/mailers/order_confirm_mailer.rb
+++ app/mailers/order_confirm_mailer.rb
@@ -4,20 +4,23 @@ include Resque::Mailer
 
   def order_placed(order_id)
     @order = Order.includes(:user, order_products: :product_size).find(order_id)
+    @id = @order.id
     @email = @order.user.email
-    mail(to: @email, subject: "Your order #{@order.id} is successfully placed.")
+    mail(to: @email, subject: "Your order #{@id} is successfully placed.")
   end
 
   def order_confirm(order_id)
     @order = Order.includes(:user, order_products: :product_size).find(order_id)
+    @id = @order.id
     @email = @order.user.email
-    mail(to: @email, subject: 'Your order #{@order.id} is confirmed')
+    mail(to: @email, subject: "Your order #{@id} is confirmed")
   end
 
   def order_onhold(order_id)
     @order = Order.find(order_id)
+    @id = @order.id
     @email = @order.user.email
-    mail(to: @email, subject: 'Order no. #{@order.id} has been put on hold by our Partner Retailer')
+    mail(to: @email, subject: "Order no. #{@id} has been put on hold by our Partner Retailer")
   end
 
   # def order_packed(order_id)
@@ -28,20 +31,23 @@ include Resque::Mailer
 
   def order_shipped(order_id)
     @order = Order.includes(:user, order_products: :product_size).find(order_id)
+    @id = @order.id
     @email = @order.user.email
-    mail(to: @email, subject: 'Your order #{@order.id} is out for delivery')
+    mail(to: @email, subject: "Your order #{@id} is out for delivery")
   end
 
   def order_delivered(order_id)
     @order = Order.find(order_id)
+    @id = @order.id
     @email = @order.user.email
-    mail(to: @email, subject: 'Your order #{@order.id} has been delivered.')
+    mail(to: @email, subject: "Your order #{@id} has been delivered.")
   end
   
   def order_cancelled(order_id)
     @order = Order.find(order_id)
+    @id = @order.id
     @email = @order.user.email
-    mail(to: @email, subject: 'Order no. #{@order.id} has been cancelled by you.')
+    mail(to: @email, subject: "Order no. #{@id} has been cancelled by you.")
   end
 
 end
